7f08342947db66b57ad9c71b891154c64efd2579
sdh0718 <sdh0718@gmail.com>
2023-03-21 12:33:49 +0900

    push

---
+++ js/index.js
@@ -0,0 +1,49 @@
+$(".mo-navi").click(function () {
+   $(".mo-navi-view").show();
+});
+$(".mo-navi-view .close").click(function () {
+   $(".mo-navi-view").hide();
+});
+
+$(".mo-navi-view .nav-wrap a").click(function () {
+   $(".mo-navi-view").hide();
+});
+
+$(".popup").hide();
+
+$(".request-btn").click(function () {
+   $(".popup.request").show();
+});
+$(".popup.request .close").click(function () {
+   $(".popup.request").hide();
+});
+
+$(".popup, .mo-navi-view").on("scroll touchmove mousewheel", function (e) {
+   e.preventDefault();
+   e.stopPropagation();
+});
+
+var WWidth = $(window).width();
+var Wheight = $(window).height();
+
+$(window).scroll(function () {
+   if (WWidth < 992 && $(this).scrollTop() > 500) {
+      $(".more-area").fadeOut();
+   }
+});
+
+function openChatroom() {
+   var windowWidth = $(window).width();
+
+   if (windowWidth < 992) {
+      $("#topMove").hide();
+   } else {
+      $(window).scroll(function () {
+         if ($(this).scrollTop() > 700) {
+            $("#topMove").fadeIn();
+         } else {
+            $("#topMove").fadeOut();
+         }
+      });
+   }
+}
