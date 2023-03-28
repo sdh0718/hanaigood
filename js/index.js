aa851d34ed553217678577cddcbda97797366feb
sdh0718 <sdh0718@gmail.com>
2023-03-28 09:10:25 +0900

    push

--- js/index.js
+++ js/index.js
@@ -1,33 +1,26 @@
-$(".mo-navi").click(function () {
-   $(".mo-navi-view").show();
-});
-$(".mo-navi-view .close").click(function () {
-   $(".mo-navi-view").hide();
-});
+window.addEventListener("DOMContentLoaded", () => {
+   $(".mo-navi").click(function () {
+      $(".mo-navi-view").show();
+   });
+   $(".mo-navi-view .close").click(function () {
+      $(".mo-navi-view").hide();
+   });
 
-$(".mo-navi-view .nav-wrap a").click(function () {
-   $(".mo-navi-view").hide();
-});
+   $(".mo-navi-view .nav-wrap a").click(function () {
+      $(".mo-navi-view").hide();
+   });
 
-$(".popup").hide();
+   $(".popup").hide();
 
-$(".request-btn").click(function () {
-   $(".popup.request").show();
-});
-$(".popup.request .close").click(function () {
-   $(".popup.request").hide();
-});
+   $(".request-btn").click(function () {
+      $(".popup.request").show();
+   });
+   $(".popup.request .close").click(function () {
+      $(".popup.request").hide();
+   });
 
-$(".popup, .mo-navi-view").on("scroll touchmove mousewheel", function (e) {
-   e.preventDefault();
-   e.stopPropagation();
-});
-
-var WWidth = $(window).width();
-var Wheight = $(window).height();
-
-$(window).scroll(function () {
-   if (WWidth < 992 && $(this).scrollTop() > 500) {
-      $(".more-area").fadeOut();
-   }
+   $(".popup, .mo-navi-view").on("scroll touchmove mousewheel", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+   });
 });
